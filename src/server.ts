04efbf68f0df73eb844782f354a44bc18d81925d
subikstha <subsatch@gmail.com>
2026-07-19 16:17:14 +0545

    feat: add a basic server

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/health', (req, res) => {
+    res.json({
+        message: "world"
+    }).status(200)
+})
+
+export {app}
+
+export default app
