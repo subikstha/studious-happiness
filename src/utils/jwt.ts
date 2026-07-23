bf3f2ce794426403b5cc24f1ca6bdc8fd360d449
subikstha <subsatch@gmail.com>
2026-07-23 14:29:27 +0545

    feat: jwt verification utility

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,4 +1,4 @@
-import { SignJWT, type JWTPayload } from "jose";
+import { jwtVerify, SignJWT } from "jose";
 import { createSecretKey } from "node:crypto";
 import env from "../../env.ts";
 
@@ -8,7 +8,7 @@ export interface JwtPayload {
   username: string;
 }
 
-export const generateToken = (payload: JWTPayload) => {
+export const generateToken = (payload: JwtPayload) => {
   const secret = env.JWT_SECRET;
   const secretKey = createSecretKey(secret, "utf-8");
 
@@ -18,4 +18,11 @@ export const generateToken = (payload: JWTPayload) => {
     .setIssuedAt()
     .setExpirationTime(env.JWT_EXPIRES_IN || "7d")
     .sign(secretKey);
+};
+
+export const verifyToken = async (token: string): Promise<JwtPayload> => {
+  const secretKey = createSecretKey(env.JWT_SECRET, "utf-8");
+  const { payload } = await jwtVerify(token, secretKey);
+
+  return payload as unknown as JwtPayload;
 };
