b772dd7c3d7990411e38ae380fae2c8c75803674
subikstha <subsatch@gmail.com>
2026-07-23 14:37:05 +0545

    feat: authentication middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,27 @@
+import type { Request, Response, NextFunction } from "express";
+import { verifyToken, type JwtPayload } from "../utils/jwt.ts";
+
+export interface AuthenticatedRequest extends Request {
+  user?: JwtPayload;
+}
+
+export const authenticateToken = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ error: "Bad Request" });
+    }
+
+    const payload = await verifyToken(token);
+    req.user = payload;
+    next();
+  } catch (e) {
+    return res.status(403).json({ error: "Forbidden" }); // 403 is for forbidden
+  }
+};
